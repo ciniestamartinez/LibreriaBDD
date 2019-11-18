6545afe265a506b9b66f5a146c75ced1c268094d
Carlos <carlosiniesta.apps@gmail.com>
2019-11-18 13:43:21 +0100

    create User and Book models

--- app/User.php
+++ app/User.php
@@ -2,29 +2,27 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function __construct(Request $request){
+        $this->request = $request;
+        $this->user = new User;
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function books(){
+        return $this->belongsToMany('App\Book');
+    }
+
+    public function create(Request $request){
+        $user = new User($request);
+        $this->user->name = $request->name;
+        $this->user->email = $request->email;
+        $this->user->password = $request->password;
+        $this->user->save();
+    }
 }
