c9f7bc8af4dd21085f1c8ab1e5d7237cd18af6aa
Carlos <carlosiniesta.apps@gmail.com>
2019-11-18 13:59:29 +0100

    create Token class

---
+++ app/Helpers/Token.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Helpers;
+use Firebase\JWT\JWT;
+
+class Token{
+
+    private $key;
+    private $data;
+    private $algorithm;
+
+    public function __construct($data = null){
+        $this->key = 'b.ijnJ#HOHJDFSOIH5_4687fgd768bh#';
+        $this->data = data;
+        $this->algorithm = array('HS256');
+    }
+    
+    public function encode(){
+        return JWT::encode($this->data, $this->key);
+    }
+
+    public function decode($token){
+        return JWT::decode($token, $this->key, $this->algorithm);
+    }
+}
