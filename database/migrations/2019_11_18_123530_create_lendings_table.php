4fe4f1e336321ef6865b6f8a273f0da5cce13992
Carlos <carlosiniesta.apps@gmail.com>
2019-11-18 13:39:34 +0100

    create tables and .env updated

---
+++ database/migrations/2019_11_18_123530_create_lendings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLendingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lendings', function (Blueprint $table) {
+            $table->primary(['id_user','id_book']);
+            $table->unsignedInteger('id_user');
+            $table->unsignedInteger('id_book');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_book')->references('id')->on('books');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lendings');
+    }
+}
